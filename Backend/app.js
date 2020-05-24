7ae0396f9e717b618be04408ae787bf11cc19ece
Damir <damir8520@gmail.com>
2020-05-24 11:35:17 +0100

    created basic server and app files for the api to server to run.
    must implement for localshopper

---
+++ Backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const app = express();
+
+mongoose
+    .connect(process.env.MONGO_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch(() => {
+        console.log(mongoose.Error);
+
+        console.log('Connection failed!');
+    });
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+
+    next();
+});
+
+module.exports = app;
